555d2624503046aec6a81abc5cdb00bacfd51795
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-07-29 20:09:46 +0900

    skip csrf

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://n-srcoukfhwnkmi2hcs2nesuepjujifnywiaaqabi-0lu-script.googleusercontent.com', 'https://script.google.com', 'https://n-6prlwevkkot7xvsah4khgwofoktbzvfsjj6axdq-0lu-script.googleusercontent.com'
+      origins 'https://n-srcoukfhwnkmi2hcs2nesuepjujifnywiaaqabi-0lu-script.googleusercontent.com', 'https://script.google.com', 'https://n-6prlwevkkot7xvsah4khgwofoktbzvfsjj6axdq-0lu-script.googleusercontent.com', 'https://n-srcoukfhwnkmi2hcs2nesuepjujifnywiaaqabi-0lu-script.googleusercontent.com'
       resource '*', methods: :any, headers: :any
     end
 end
