de037e909a7142293e9648ab7eace6c3f09fb935
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-07-29 19:47:50 +0900

    Macにも対応させたのとCORSオリジンの追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://n-srcoukfhwnkmi2hcs2nesuepjujifnywiaaqabi-0lu-script.googleusercontent.com', 'https://script.google.com'
-      resource '*', methods: :any, headers: :any
+      origins 'https://n-srcoukfhwnkmi2hcs2nesuepjujifnywiaaqabi-0lu-script.googleusercontent.com', 'https://script.google.com', 'https://n-6prlwevkkot7xvsah4khgwofoktbzvfsjj6axdq-0lu-script.googleusercontent.com'      resource '*', methods: :any, headers: :any
     end
   end
