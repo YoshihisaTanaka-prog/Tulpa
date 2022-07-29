d55c846f523b3aa46f1e569bc57b56381ebf5cfe
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-07-29 11:22:14 +0900

    edit token data

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -6,18 +6,7 @@ class ApisController < ApplicationController
         base_uri = 'https://yoshihi-server-tulpa.firebaseio.com/'
         # 参照元↓
         # https://googleapis-dev.translate.goog/ruby/google-cloud-firestore/latest/Google/Cloud/Firestore.html?_x_tr_sl=en&_x_tr_tl=ja&_x_tr_hl=ja&_x_tr_pto=wapp#new-class_method
-        credentials_hash = {
-            "type" => ENV["type"],
-            "project_id" => ENV["project_id"],
-            "private_key_id" => ENV["private_key_id"],
-            "private_key" => ENV["private_key"],
-            "client_email" => ENV["client_email"],
-            "client_id" => ENV["client_id"],
-            "auth_uri" => ENV["auth_uri"],
-            "token_uri" => ENV["token_uri"],
-            "auth_provider_x509_cert_url" => ENV["auth_provider_x509_cert_url"],
-            "client_x509_cert_url" => ENV["client_x509_cert_url"]
-        }
+        credentials_hash = JSON.parse(ENV['GOOGLE_CLOUD_CREDENTIALS_JSON'])
 
         # render plain: credentials_hash.to_json
 
