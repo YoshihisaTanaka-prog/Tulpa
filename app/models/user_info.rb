2c900804991f89e1c82fe3e87d35faa3d8a30a20
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-08-01 19:06:01 +0900

    Update user_info.rb

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -7,7 +7,7 @@ class UserInfo < ApplicationRecord
             UserInfoDetail.where(user_info_id: self.id, category_id: category.id).each do |detail|
                 list.push detail.out
             end
-            output_details.push {type: category.type_name, details: list}
+            output_details.push({type: category.type_name, details: list})
         end
         return {id: self.id, name: self.name, isNeedSleep: self.is_need_sleep, isNeedMeal: self.is_need_meal, details: output_details}
     end
