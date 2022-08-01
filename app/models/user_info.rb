9344cd21b463326bbc72c8089d1ccaa0f61b5248
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-08-01 19:03:15 +0900

    ユーザー詳細情報のJSON値をカテゴライズ

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,10 +1,13 @@
 class UserInfo < ApplicationRecord
 
     def out
-        details = UserInfoDetail.where(user_info_id: self.id).order(:category_id)
         output_details = []
-        details.each do |detail|
-            output_details.push detail.out
+        UserInfoCategory.all.order(:id).each do |category|
+            list = []
+            UserInfoDetail.where(user_info_id: self.id, category_id: category.id).each do |detail|
+                list.push detail.out
+            end
+            output_details.push {type: category.type_name, details: list}
         end
         return {id: self.id, name: self.name, isNeedSleep: self.is_need_sleep, isNeedMeal: self.is_need_meal, details: output_details}
     end
