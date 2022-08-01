671ae18ad83593f78b070e204ca91f1615a93c30
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-08-01 17:08:01 +0900

    ユーザーの詳細情報をJSON値に追加

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,7 +1,12 @@
 class UserInfo < ApplicationRecord
 
     def out
-        return {id: self.id, name: self.name, isNeedSleep: self.is_need_sleep, isNeedMeal: self.is_need_meal}
+        details = UserInfoDetail.where(user_info_id: self.id).order(:category_id)
+        output_details = []
+        details.each do |detail|
+            output_details.push detail.out
+        end
+        return {id: self.id, name: self.name, isNeedSleep: self.is_need_sleep, isNeedMeal: self.is_need_meal, details: output_details}
     end
     
 end
--- app/models/user_info_detail.rb
+++ app/models/user_info_detail.rb
@@ -1,3 +1,7 @@
 class UserInfoDetail < ApplicationRecord
     validates :category_id, numericality: {greater_than: 0}
+
+    def out
+        return {id: self.id, text: self.text}
+    end
 end
