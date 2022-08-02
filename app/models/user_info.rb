8dc7fd52b35d58b3c629bb9ad9c2980fb0c77408
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-08-03 07:08:10 +0900

    Update user_info.rb

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,13 +1,13 @@
 class UserInfo < ApplicationRecord
 
     def out
-        output_details = []
+        output_details = {}
         UserInfoCategory.all.order(:id).each do |category|
             list = {}
             UserInfoDetail.where(user_info_id: self.id, category_id: category.id).each do |detail|
                 list[detail.id.to_s] = detail.out
             end
-            output_details.push({typeId: category.id, typeName: category.type_name, texts: list})
+            output_details[category.id.to_s] = {typeName: category.type_name, texts: list}
         end
         return {self.id.to_s => {name: self.name, isNeedSleep: self.is_need_sleep, isNeedMeal: self.is_need_meal, details: output_details}}
     end
