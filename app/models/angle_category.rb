6b5cfe51df6647c28224f3a6ea8cdc0413f44b7e
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-08-11 06:45:17 +0900

    Update angle_category.rb

--- app/models/angle_category.rb
+++ app/models/angle_category.rb
@@ -1,10 +1,10 @@
 class AngleCategory < ApplicationRecord
 
     def angles
-        angle_ids = AngleAngleCategory.where(category_id: self.id).order(:name)
+        angle_ids = AngleAngleCategory.where(category_id: self.id)
         ret = []
         angle_ids.each do |id|
-            angle = Angle.find_by(id: id)
+            angle = Angle.find_by(id: id.angle_id)
             unless angle.blank?
                 ret.push({name: angle.name})
             end
